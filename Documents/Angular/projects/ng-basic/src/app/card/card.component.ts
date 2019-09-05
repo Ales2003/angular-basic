c5e81685c78d16c852ada56adfe75f3715010822
aliaksandr vishneuski <aliaksandr_vishneuski@senla.eu>
2019-09-05 17:17:46 +0300

    second variant template declaration

--- Documents/Angular/projects/ng-basic/src/app/card/card.component.ts
+++ Documents/Angular/projects/ng-basic/src/app/card/card.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core'
 
+//Вариант 1 - указание шаблона как ссылку
 @Component({
     // куда будет вставляться компонент, app-что бы разделить на ши и не наши 
     selector: 'app-card', 
@@ -7,6 +8,33 @@ import {Component} from '@angular/core'
     templateUrl: './card.component.html',
     styleUrls: ['./card.component.css']
 })
+
+//Вариант 2 - указание шаблона в темплейт литералaх ``
+// @Component({
+//     selector: 'app-card', 
+//     template: `
+//     <div class="card">
+//         <h1>Card Component</h1>
+//         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam, in.</p>
+//     </div>
+//     `,
+//     styles: [
+//         `
+//         .card {
+//             padding: .5rem 1 rem;
+//             border: 1px dashed #ccc;
+//             margin-bottom: 1rem;
+//         }
+        
+//         .card h2  {
+//             margin-bottom: .5rem;
+//         } 
+
+//         `
+//     ]   
+// })
+
+
 export class CardComponent {
 
 }
