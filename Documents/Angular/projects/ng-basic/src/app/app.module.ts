1508466d33611b04e4be1d9614b0049e7bab24c8
aliaksandr vishneuski <aliaksandr_vishneuski@senla.eu>
2019-09-06 11:42:40 +0300

    added angular 2-w-binding

--- Documents/Angular/projects/ng-basic/src/app/app.module.ts
+++ Documents/Angular/projects/ng-basic/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
@@ -8,12 +9,15 @@ import { FormComponent } from './form/form.component';
 @NgModule({
   declarations: [
     AppComponent,
-    //перечисляем наш компонент
+    //перечисляем наши компоненти
     CardComponent,
     FormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //для 2-w байндинга
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
